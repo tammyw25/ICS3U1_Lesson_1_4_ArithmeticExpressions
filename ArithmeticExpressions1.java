c86d5b3209e00035bc31a276010749a8e94a9239
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2024-02-07 18:52:39 -0500

    Create ArithmeticExpressions1.java

---
+++ ArithmeticExpressions1.java
@@ -0,0 +1,27 @@
+class ArithmeticExpressions1 extends ConsoleProgram {
+
+  /**
+  * Day Age Calculator
+  */
+  public void run() {
+    
+    int intAge;
+    int intDays;
+
+    intAge = readInt("How old are you? ");
+    intDays = intAge * 365;
+
+    //System.out.print("You have been alive for this many days: ");
+    System.out.println("You have been alive for this many days: " + intDays);
+    //System.out.println(intDays);
+
+
+    // divide an int by an int will always be an int
+
+    // divide an int by a double will always be a double
+
+    // divide an int by a double will always be a double
+    
+
+  }
+}

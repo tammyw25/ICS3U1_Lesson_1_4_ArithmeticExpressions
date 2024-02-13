f959fffff6248b8da4ef675211d296f4a872e476
tammyw25 <tammy.wong25@ycdsbk12.ca>
2024-02-13 14:25:32 -0500

    UpdatedArithmeticExpression

--- ArithmeticExpressions1.java
+++ ArithmeticExpressions1.java
@@ -15,11 +15,20 @@ class ArithmeticExpressions1 extends ConsoleProgram {
     System.out.println("You have been alive for this many days: " + intDays);
     //System.out.println(intDays);
 
+    int intNum1;
+    int intNum2;
+    double dblNum3;
+    double dblNum4;
 
-    // divide an int by an int will always be an int
+    intNum1 = 106;
+    intNum2 = 42;
+    dblNum3 = 3.1415;
+    dblNum4 = 4.5;
 
+    // divide an int by an int 15will always be an int
+    System.out.println(intNum1/intNum2);
     // divide an int by a double will always be a double
-
+    System.out.println(dblNum3/dblNum4);
     // divide an int by a double will always be a double
     
 
